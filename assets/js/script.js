77d62611fc42e05a3d1baeb8052b2369e80c9e64
Narek <narek700@mail.ru>
2022-03-19 15:58:33 +0400

    menu changes & home page adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,70 @@ $(document).ready(function () {
         centerMode: true,
         autoplaySpeed: 4000,
     });
+
+    $('.second-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+    $('#third').slick({
+        infinite: true,
+        slidesToShow: 4,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: true,
+                }
+            }
+        ]
+    });
+
+    //tablet-mobile language change
+    $('#btnChange').on('click', function () {
+        $('#languageMenu').toggleClass('active');
+        $('body').toggleClass('overflow-hidden')
+    });
+
+    //menu toggle for mobile-tablet
+    $('#btnMenu').on('click', function () {
+        $(this).toggleClass('active');
+        $('#nav').toggleClass('active')
+    })
+
+    //menu links click for mobile-tablet
+    $('#nav a').on('click', function (e) {
+        e.preventDefault();
+    });
 });
